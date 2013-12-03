0a737e012ec89e67a24b1227e4f1d2030e69f695
Nikola Mihailovic <mihailovicnikola33@gmail.com>
2013-12-03 23:01:08 +0100

    kod za skidanje slika

--- rak.rb
+++ rak.rb
@@ -13,17 +13,17 @@ class Rakuten
 		sheet = book.create_worksheet
 		red = 0
 		paginatori = ["http://global.rakuten.com/en/search/?f=2&k=&p=2&sid=crouka",
-					        "http://global.rakuten.com/en/search/?f=2&k=&p=3&sid=crouka",
-					        "http://global.rakuten.com/en/search/?f=2&k=&p=4&sid=crouka"]
+					        "http://global.rakuten.com/en/search/?f=2&k=&p=3&sid=crouka"]
 		paginatori.each do |pag|
-			visit pag 			        
+			visit pag 
+
 
 			ime_linka = []
 			all(:xpath, '//div[@class="b-content b-fix-2lines"]/a').each do |link|         
 				ime_linka << link[:href]                                                   
 			end
 
-			ime_linka.each do |link|
+			ime_linka[0..2].each do |link|
 				visit link
 
 			 	if page.has_selector?(:xpath, '//*[@itemprop="name"]')
@@ -47,12 +47,43 @@ class Rakuten
 				if page.has_selector?(:xpath, '//span[@class="fabric"]')
 					sheet[red,6] = find(:xpath, '//span[@class="fabric"]').text
 				end
+				if page.has_selector?('//*[@id="main_image"]')
+					url_slike = find('//*[@id="main_image"]')[:src]
+						open(url_slike) do |f|
+		  				File.open(url_slike,"wb") do |file|
+			     			file.puts f.read
+			  			end
+						end
+				end
 
-				red +=1
+
+				red += 1
 			end
 		end
 		book.write "rak.xls"
 	end
+
+	def test
+		stranice = ["http://global.rakuten.com/en/store/crouka/item/250672/",
+								"http://global.rakuten.com/en/store/crouka/item/the_smock_shop_ss_8157_pl/"]
+		brojac = 1						
+		stranice.each do |stranica|
+			visit stranica
+
+			if page.has_selector?('//*[@class="b-main-image"]/a')
+				url_slike = find('//*[@class="b-main-image"]/a')[:href]
+				open(url_slike) do |f|
+	  			File.open("test#{brojac}.jpg","wb") do |file|
+		     		file.puts f.read
+		  		end
+				end
+			end	
+
+			brojac += 1	
+		end
+
+
+	end 
 end
 a = Rakuten.new
-a.go
+a.test
